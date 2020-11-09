0bb96461c7f6d8ccf1104b17827cff64310764f8
Taras <taras.korovytskyi.itis.2020@lpnu.ua>
2020-11-09 09:23:57 +0200

    5-6

---
+++ 5.6/5.6.cpp
@@ -0,0 +1,20 @@
+﻿// 5.6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double a, const double b);
+int main()
+{
+	double s, t, a;
+
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = (h(1, s * t) + (h)(t + s, 1));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double a, const double b)
+{
+	return ((a * a) - (sin(b) * cos(a)) + b);
+}
